0d6738c0269db2299e92a830353220714e700c46
Kizito Daka <kizitodak@gmail.com>
2026-05-04 15:25:55 +0100

    Updated build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ set -o errexit   # exit on any error
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
