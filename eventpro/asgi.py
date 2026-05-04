7ae5328706504aaf0e9a2a8b3764a360c215e928
Kizito Daka <kizitodak@gmail.com>
2026-05-04 16:04:02 +0100

    updateeeeiii wiz

---
+++ eventpro/asgi.py
@@ -0,0 +1,20 @@
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventpro.settings')
+
+import django
+django.setup()
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from checkin.routing import websocket_urlpatterns
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
